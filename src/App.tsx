e1abbe779ef213a75e63e1d8ff8aeb70b8146a93
Ehsan Morshedzadeh <Ehsan.morshedzadeh@gmail.com>
2023-07-21 14:03:13 +0330

    "react router dom added"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import LandingPage from './pages/LandingPage'
 import './style/App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+      </Routes>
     </>
   )
 }
